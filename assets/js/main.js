3b89c4ee9296321d21d0fd3f515687dc9a6c0a20
AbdoHesham <57379925+AbdoHesham@users.noreply.github.com>
2020-10-22 23:40:26 +0200

    update 22.10.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,30 +32,30 @@ function saveToLocal(e) {
 }
 var modal = document.getElementById("myModal");
 var nav = document.querySelector(".nav");
-var side= document.querySelector("#side");
-var imgLogo=document.querySelector("#imgLogo");
+var side = document.querySelector("#side");
+var imgLogo = document.querySelector("#imgLogo");
 
 function openModal() {
-  console.log(localStorage );
+  console.log(localStorage);
   setTimeout(function () {
-    if (!localStorage.getItem("email","password")) {
-      nav.style.display="none";
-      side.style.display="none";
-      imgLogo.style.display="none";
+    if (!localStorage.getItem("email", "password")) {
+      nav.style.display = "none";
+      side.style.display = "none";
+      imgLogo.style.display = "none";
       modal.style.display = "block";
-      
+
     } else {
-      
-      console.log(localStorage );
+
+      console.log(localStorage);
     }
   }, 3000);
 
 }
 
 function close() {
-  nav.style.display="block";
-  side.style.display="block";
-  imgLogo.style.display="block";
+  nav.style.display = "block";
+  side.style.display = "block";
+  imgLogo.style.display = "block";
   modal.style.display = "none";
 }
 
@@ -78,3 +78,24 @@ function allertErr() {
   }, 3000);
 }
 
+/////////////////////////////// slider/////////////////////////////////////////
+var imgs = ["https://images.pexels.com/photos/1432066/pexels-photo-1432066.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+  "https://images.pexels.com/photos/2113566/pexels-photo-2113566.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  "https://images.pexels.com/photos/142497/pexels-photo-142497.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", 
+  "https://images.pexels.com/photos/1188849/pexels-photo-1188849.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+];
+
+document.onkeydown = function (event) {
+  var e = event || window.event;
+  if (e.keyCode == '37') { //LEFT
+    changeImage(-1);
+  } else if (e.keyCode == '39') { //RIGHT
+    changeImage(1);
+  }
+}
+
+function changeImage(dir) {
+  var img = document.getElementById("imgClickAndChange");
+  img.src = imgs[imgs.indexOf(img.src) + dir] || imgs[(dir == 1) ? 0 : imgs.length - 1];
+}
